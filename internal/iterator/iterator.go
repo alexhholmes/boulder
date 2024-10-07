55089306fa3ad66fd01e27dad49d86dcecd44188
Alex Holmes <alex@sed.dev>
2024-10-07 00:23:29 -0400

    Add todo

--- internal/iterator/iterator.go
+++ internal/iterator/iterator.go
@@ -7,9 +7,15 @@ import (
 )
 
 type Iterator interface {
+	// SeekGE(key []byte, flags SeekGEFlags) *InternalKV
+	// SeekPrefixGE(prefix, key []byte, flags SeekGEFlags) *InternalKV
+	// SeekLT(key []byte, flags SeekLTFlags) *InternalKV
 	First() *base.InternalKV
 	Last() *base.InternalKV
 	Next() *base.InternalKV
+	// NextPrefix(succKey []byte) *InternalKV
 	Prev() *base.InternalKV
+	// Error() error
+	// SetBounds(lower, upper []byte)
 	io.Closer
 }
